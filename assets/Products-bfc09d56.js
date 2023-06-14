4068238bdcf89a78fbd972fa0ba5b50a9d7b4254
HEKEPOIU <decnovpk92805@gmail.com>
2023-06-15 03:06:29 +0800

    change

---
+++ assets/Products-bfc09d56.js
@@ -0,0 +1,33 @@
+import{o as e,f as s,w as c,V as d,c as i,r as _,F as p,b as u,e as a,g as n,h as o,t as l,a as g}from"./index-d2661475.js";const f="/finalWeb/assets/product0-0c045ebf.png",h="/finalWeb/assets/product1-db6ac8c2.png",x="/finalWeb/assets/product2-9e2eff2b.png",b="/finalWeb/assets/product3-7ea910f1.png";const y={class:"bg-blue-light-ss text-h5 text-white text-center boxresize1 mt-5",style:{"white-space":"pre-wrap"}},w={class:"bg-blue-light-s1 py-10 pl-5 text-h6 mt-5 boxresize textcolor",style:{"white-space":"pre-wrap"}},z={class:"bg-blue-light-ss text-h5 text-white text-center flex-grow-1 mt-5 line_space",style:{"white-space":"pre-wrap"}},H={class:"bg-blue-light-s1 py-2 pl-2 text-h6 mt-5 textcolor",style:{"white-space":"pre-wrap"}},V={__name:"Products",setup(A){const m=[["@/assets/product/product0.png",`Dim:Ø31x11Hmm
+USB 介面`,`特色 :
+1. AI 高效能降噪
+2. Al 自動模型辨識
+3. 可重構 A | 模型
+4. 取樣頻率 4KHz
+5. 最大頻寬 :
+0.0095Hz ~ 10H 1Hz—1KHz
+6 · 解析度 20bit`],["./assets/product/product1.png",`Dim:14Wx 16Lx6H mm
+USB 介面`,`特色 :
+1. AI 高效能降噪
+2.Al 自動模型辨識 
+3. 可重構 AI 模型
+5. 取樣頻率 16KHz
+6, 大寬 6KHz
+7. 超小尺寸`],["./assets/product/product2.png",`Dim:Ø9 × 33 , 2H mm
+IJSB 介面`,`特色 :
+1. AI noise reduction
+2. Al model Recognition
+3. Reconfig u rable AI model
+4. SampIing frequency 16KHz
+5. M ax. bandwidth 6KHz
+6. Resol ution 16bit
+7. Screw type`],["./assets/product/product3.png",`Di m:90L x70W x28H mm
+ WiFi/ BT/Ethernet/
+ EtherCAT (Optional)
+ Vibration sensor X2
+ Thermal sensor X5
+ Hall (RPM) sensorxl
+ DI x8, DO x 2`,`特色 :
+1. Smart learning
+2. AI model training
+3. Reconfigurable AI model`]];return(I,k)=>(e(),s(d,{class:"d-flex my-s justify-space-evenly",height:"500px"},{default:c(()=>[(e(),i(p,null,_(m,(r,t)=>(e(),i(p,null,[t<3?(e(),s(u,{key:0,cols:"2",class:"d-flex flex-column justify-end"},{default:c(()=>[t==0?(e(),s(a,{key:0,src:f,class:"imgresize"})):n("",!0),t==1?(e(),s(a,{key:1,src:h,class:"imgresize"})):n("",!0),t==2?(e(),s(a,{key:2,src:x,class:"imgresize"})):n("",!0),o("div",y,l(r[1]),1),o("div",w,l(r[2]),1)]),_:2},1024)):n("",!0),t==3?(e(),s(u,{key:1,cols:"2",class:"d-flex flex-column justify-end"},{default:c(()=>[g(a,{src:b,class:"imgresize flex-grow-0"}),o("div",z,l(r[1]),1),o("div",H,l(r[2]),1)]),_:2},1024)):n("",!0)],64))),64))]),_:1}))}};export{V as default};
